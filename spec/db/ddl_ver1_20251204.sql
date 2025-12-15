0273698b1678eda687bce34dc5e8cf9caa723128
Takahara <kosuke.takahara@yourstand-ev.com>
2025-12-16 00:34:45 +0900

    足りなかったtriggerを追加 (汎用的なtimestamp update_at)

--- spec/db/ddl_ver1_20251204.sql
+++ spec/db/ddl_ver1_20251204.sql
@@ -1,3 +1,15 @@
+create or replace function public.set_current_timestamp_updated_at()
+returns trigger
+language plpgsql
+as $$
+begin
+  new.updated_at = now();
+  return new;
+end;
+$$;
+
+
+
 -- ===============================
 -- 1. プロフィール
 -- ===============================
